c840ab2d71ff8f4f118ac98dc7d00bff3b400e71
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2025-03-24 19:25:43 +0800

    fix: outline style

--- src/components/common/DialogWrapper.vue
+++ src/components/common/DialogWrapper.vue
@@ -4,9 +4,15 @@
     class="modal"
     @close="isOpen = false"
   >
+    <form
+      method="dialog"
+      class="modal-backdrop"
+    >
+      <button class="!outline-none">close</button>
+    </form>
     <div class="modal-box relative max-h-[90dvh] overflow-hidden p-0 max-md:max-h-[70dvh]">
       <form method="dialog">
-        <button class="btn btn-circle btn-ghost btn-xs absolute top-1 right-1 z-10 outline-hidden">
+        <button class="btn btn-circle btn-ghost btn-xs absolute top-1 right-1 z-10">
           <XMarkIcon class="h-4 w-4" />
         </button>
       </form>
@@ -16,12 +22,6 @@
         <slot></slot>
       </div>
     </div>
-    <form
-      method="dialog"
-      class="modal-backdrop"
-    >
-      <button>close</button>
-    </form>
   </dialog>
 </template>
 
