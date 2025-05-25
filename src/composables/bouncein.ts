e2938326b98b8ed9ffd62a66fb7121569df2f479
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2025-05-26 01:23:30 +0800

    fix: freezing issue when scroll animation is on

--- src/composables/bouncein.ts
+++ src/composables/bouncein.ts
@@ -4,6 +4,7 @@ import { useCurrentElement, useElementVisibility } from '@vueuse/core'
 import { onMounted, watch, type Ref } from 'vue'
 
 const className = 'bounce-in'
+const initClassName = ['scale-85', 'opacity-0']
 
 export function useBounceOnVisible(el: Ref<HTMLElement> = useCurrentElement<HTMLElement>()) {
   if (!isMiddleScreen.value || !scrollAnimationEffect.value) return
@@ -13,7 +14,7 @@ export function useBounceOnVisible(el: Ref<HTMLElement> = useCurrentElement<HTML
   onMounted(() => {
     if (!el.value) return
 
-    el.value.style.opacity = '0'
+    el.value.classList.add(...initClassName)
 
     watch(
       visible,
@@ -21,11 +22,11 @@ export function useBounceOnVisible(el: Ref<HTMLElement> = useCurrentElement<HTML
         if (!el.value) return
 
         if (value) {
-          el.value?.classList.add(className)
-          el.value!.style.opacity = 'unset'
+          el.value.classList.add(className)
+          el.value.classList.remove(...initClassName)
         } else {
-          el.value?.classList.remove(className)
-          el.value!.style.opacity = '0'
+          el.value.classList.remove(className)
+          el.value.classList.add(...initClassName)
         }
       },
       { immediate: true },
